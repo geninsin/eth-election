f07adf938128d9c661e6a96794dfa1c9e7074b94
geninsin <sergey.furtakov@gmail.com>
2018-03-13 18:26:42 -0500

    voters casting

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -16,6 +16,9 @@ contract Election {
 	// Store a Candidates Count
 	uint public candidatesCount;
 
+	// Store acconut that have voted
+	mapping( address => bool) public voters;
+
 	// Constructor
 	function Election () public {
 		addCandidate( "Candidate 1");
@@ -25,5 +28,20 @@ contract Election {
 	function addCandidate (string _name) private {
 		candidatesCount++;
 		candidates[candidatesCount] = Candidate( candidatesCount, _name, 0 );
+	}
+
+	function vote (uint _candidateId) public {
+
+		// Require that they never voted before		
+		require( !voters[msg.sender] );
+
+		// Requre a valid candidate
+		require( _candidateId > 0 && _candidateId <= candidatesCount );
+
+		// Record that the voter has voted
+		voters[msg.sender] = true;
+
+		// Update Canddates' vote count
+		candidates[_candidateId].voteCount++;
 	}
 }
