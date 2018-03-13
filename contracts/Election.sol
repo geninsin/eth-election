f6b6d7ec34d85bcab3e111470d88570b8885f8ad
geninsin <sergey.furtakov@gmail.com>
2018-03-12 19:49:34 -0500

    test contract

---
+++ contracts/Election.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.2;
+
+contract Election {
+	
+	// Store a candidate
+	// Read a candidate
+
+	string public candidate;
+	
+	// Constructor
+	function Election () public {
+		candidate = "Candidate 1";
+	}
+
+
+
+}
